dfde88ab78c28d0c7646a361aafb98f6160ebc8b
Noah Hicks <77810108+noahg1@users.noreply.github.com>
2024-09-05 11:06:56 -0400

    installing latest version

--- vault-shim-installer.sh
+++ vault-shim-installer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-version=$1
+git fetch --tags
+latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
 # turn to all lowercase and then capitalize the first letter of the OS name
 os="$(uname -s | tr '[:upper:]' '[:lower:]' | awk '{ print toupper(substr($0, 1, 1)) substr($0, 2) }')"
@@ -15,6 +16,6 @@ echo "os: $os"
 echo "arch: $arch"
 
 # purposefully leaving the filename on the tar so that we know which shim was downloaded if we ever have to shell in and diagnose an issue
-curl -L "https://github.com/Bandwidth/vault-shim/tarball/v${version}" --output "/usr/local/bin/vault-shim_${os}_${arch}.tar.gz"
+curl -L "https://github.com/Bandwidth/vault-shim/tarball/${latestTag}" --output "/usr/local/bin/vault-shim_${os}_${arch}.tar.gz"
 
 tar xzf "/usr/local/bin/vault-shim_${os}_${arch}.tar.gz" -C "/usr/local/bin"
